1a2fbeb0bb8931dd4efbd641ddea180fc340aa8b
Harpreet Singh <61841284+iharpreetbhatti@users.noreply.github.com>
2023-04-05 12:21:01 +0530

    added custom fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        opensans: ["Open Sans", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"]
+      }
+    }
   },
   plugins: []
 }
