a26b7ee4f05233a7415c803671963cca67b2ca1a
Harpreet Singh <61841284+iharpreetbhatti@users.noreply.github.com>
2023-04-14 09:50:25 +0530

    removed background image from header

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="bg-curve-mobile lg:bg-curve-desktop bg-no-repeat bg-bottom w-full">
+      <header className="header">
         <div className="container mx-auto px-6">
           <Navbar data={data} />
           <Hero data={data} />
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function MainContent() {
+  return (
+    <React.Fragment>
+      <section className="main-content"></section>
+    </React.Fragment>
+  )
+}
