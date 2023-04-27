0edd4e31d16b2d872ca0e37bf6177c81abb9becb
Harpreet Singh <61841284+iharpreetbhatti@users.noreply.github.com>
2023-04-27 20:40:57 +0530

    added image to other details section

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import SectionContent from "./SectionContent"
+import ImageURL from "../assets/illustration-2.svg"
 
 export default function MainContent({ data }) {
   const content = data ? data.blockText[1] : undefined
   return (
     <section className="main-content">
+      <img
+        src={ImageURL}
+        alt="Illstration 2"
+      />
       {data && (
         <SectionContent
           title={content.title}
