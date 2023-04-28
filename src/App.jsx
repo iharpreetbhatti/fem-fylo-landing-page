ccc74620ac56242e46279d3af370c107b13899c5
Harpreet Singh <61841284+iharpreetbhatti@users.noreply.github.com>
2023-04-28 08:26:37 +0530

    imported data from data.json directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
+import data from "./data/data.json"
 
 function App() {
+  console.log(data)
   return (
     <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
+      <Header content={data.header} />
+      {/* <MainContent content={data.mainContent} />
+      <Footer content={data.footer} /> */}
     </div>
   )
 }
